b8188875a0552241fc2a5ef5ae557f4603f06ee1
ShreeSub <rsl2171997@gmail.com>
2021-10-31 13:06:45 -0400

    adding main menu

--- LoyalityProgram/src/MainMenu.java
+++ LoyalityProgram/src/MainMenu.java
@@ -2,4 +2,86 @@ import java.sql.*;
 import java.util.*;
 
 public class MainMenu {
+
+
+    public static final String jdbcURL = "jdbc:oracle:thin:@ora.csc.ncsu.edu:1521:orcl01";
+    public static final String user = "araghut";
+    public static final String passwd = "200421320";
+
+    public static Connection connection = null;
+    public static Statement statement = null;
+
+    public static void main(String[] args) {
+        try {
+
+            // Load the driver. This creates an instance of the driver
+            // and calls the registerDriver method to make Oracle Thin
+            // driver available to clients.
+
+            Class.forName("oracle.jdbc.OracleDriver");
+
+            try {
+
+                // Get a connection from the first driver in the
+                // DriverManager list that recognizes the URL jdbcURL
+
+                connection = DriverManager.getConnection(jdbcURL, user, passwd);
+                statement = connection.createStatement();
+
+                System.out.println("\t\tWelcome to Customer Loyalty Program.\n\n");
+                showMenu();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                exitProgram();
+            }
+        } catch (Throwable oops) {
+            oops.printStackTrace();
+        }
+    }
+
+    public static void showMenu() {
+        Scanner sc = new Scanner(System.in);
+
+        int selection = 1;
+
+        do {
+            System.out.println("Select from the options below");
+            System.out.println("1. Login");
+            System.out.println("2. Sign Up");
+            System.out.println("3. Show Queries");
+            System.out.println("4. Exit");
+            System.out.print("Enter your option:");
+
+            try {
+                selection = sc.nextInt();
+
+                switch (selection) {
+                    case 1:
+                        Login.loginUI();
+                        break;
+                    case 2:
+                        Signup.signUpUI();
+                        break;
+                    case 3:
+                        //Todo: Show Queries
+                        break;
+                    case 4:
+                        exitProgram();
+                        break;
+                    default:
+                        System.out.println("You have entered a wrong option. Please choose again.");
+                }
+            } catch (Exception e) {
+                System.out.println("You have entered a wrong option. Please choose again.");
+                showMenu();
+            }
+        } while (selection != 4);
+    }
+
+    public static void exitProgram() {
+        Utility.close(connection);
+        Utility.close(statement);
+        System.exit((0));
+    }
 }
