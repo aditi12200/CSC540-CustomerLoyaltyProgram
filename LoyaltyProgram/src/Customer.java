98c4b4c4d3ef047fd63d83bb539afe9ad53e3557
Akshaya Raghuthaman <akshayakoroth.ak@gmail.com>
2021-11-13 23:00:47 -0500

    Update Customer.java

--- LoyaltyProgram/src/Customer.java
+++ LoyaltyProgram/src/Customer.java
@@ -902,7 +902,6 @@ public class Customer {
         //from the RRR table figure out which rewards are supported for this brand and display that
         try
         {
-            System.out.println(str);
             String SQL_Wallet_rp = "SELECT R.REWARD_NAME, MAX(S.VERSION_NO) AS MAX_VERSION" +
                     " FROM REWARD_TYPE R, RR_RULES S" +
                     " WHERE S.BRAND_ID = '"+brandId+"' AND R.RT_ID = S.REWARD_CATEGORY_CODE AND R.RT_ID IN " + str +
